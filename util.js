03be78fabb0ed84f39cead605532cdf75d23e62c
dezhizhang <1541609448@qq.com>
2019-06-16 14:23:43 +0800

    三角函数

--- util.js
+++ util.js
@@ -16,3 +16,13 @@ C.eventWrapper = function(ev) {
     let {left,top} = target.getBoundingClientRect();
     return {x:pageX-left,y:pageY-top}
 }
+
+//用度转弧度
+C.toRed = function(angle) {
+    return angle * Math.PI / 180;
+}
+//孤度转角度
+C.toAngle = function(red) {
+    return red * 180 / Math.PI
+}
+
